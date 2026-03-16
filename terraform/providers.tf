a8d1f1f53b083609138584f29fe801eb37fd4edd
CarlosARAlmeida <carlos.guto@gmail.com>
2026-03-16 19:15:53 -0300

    ULtimo do dia

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,14 +2,12 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "3.0.1-rc4" # Versão estável e compatível
+      version = "3.0.1-rc4"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_api_token_id     = var.proxmox_api_token_id # Note que o nome mudou um pouco
-  pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure     = true
+  # Deixe vazio. O provider vai ler as envs PM_... diretamente.
+    pm_tls_insecure = true
 }
