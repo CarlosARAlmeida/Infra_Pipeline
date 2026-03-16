cd84c8d680a80f123827fc9097c191a557e1650d
CarlosARAlmeida <carlos.guto@gmail.com>
2026-03-16 18:47:29 -0300

    feat: trigger iac pipeline via self-hosted runner

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,15 @@
 variable "proxmox_api_url" {
   type        = string
-  description = https://192.168.98.38:8006/api2/json
+  description = "URL da API do Proxmox (Ex: https://ip-do-servidor:8006/api2/json)"
 }
 
 variable "proxmox_api_token" {
   type        = string
-  description = "Token de API (ID=Secret)"
+  description = "Token de API do Proxmox"
   sensitive   = true
 }
 
 variable "ssh_public_key" {
   type        = string
-  description = "Sua chave pública para injetar nas VMs via Cloud-init"
+  description = "Chave publica para as VMs"
 }
