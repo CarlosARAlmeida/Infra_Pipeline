dc60b1d5a99e0b9d72eee91d861ef872c019b508
CarlosARAlmeida <carlos.guto@gmail.com>
2026-03-17 14:32:19 -0300

    teste8

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,7 +15,7 @@ provider "proxmox" {
   pm_api_token_id     = "terraform-prov@pve!terraform-token"
   
   # O Secret UUID (Aquele que começa com ef8cc0f4...)
-  pm_api_token_secret = "ef8cc0f4-f48a-46el-a6b4-91fa7b2b15c6"
+  pm_api_token_secret = "ef8cc0f4-f48a-46e1-a6b4-91fa7b2b15c6"
 
   pm_tls_insecure     = true
   # Remova ou comente as outras linhas que estavam aqui
