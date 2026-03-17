940462233e83896a8e5cda6f01e61cfe068dfdd1
CarlosARAlmeida <carlos.guto@gmail.com>
2026-03-17 14:53:48 -0300

    13

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,23 +2,16 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "3.0.1-rc4" # Versão estável que estávamos usando
+      version = "2.9.14" # <--- ESTA VERSÃO É A SOLUÇÃO
     }
   }
 }
 
 provider "proxmox" {
-  # URL completa com /api2/json
-  pm_api_url          = "https://192.168.98.38:8006"
-  
-  # ID completo: usuario@realm!nome_do_token
+  pm_api_url          = "https://192.168.98.38:8006/api2/json"
   pm_api_token_id     = "terraform-prov@pve!terraform-token"
-  
-  # O Secret UUID (Aquele que começa com ef8cc0f4...)
-  pm_api_token_secret = "e24857b0-6794-4a33-9a28-63bfdf580a8b"
-
+  pm_api_token_secret = "ef8cc0f4-f48a-46e1-a6b4-91fa7b2b15c6"
   pm_tls_insecure     = true
-  # Remova ou comente as outras linhas que estavam aqui
 }
 
 
