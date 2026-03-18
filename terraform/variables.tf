7ec896ad21b9a8dd1d4dda0a26893eca5c0d9a4b
CarlosARAlmeida <carlos.guto@gmail.com>
2026-03-18 16:10:12 -0300

    25

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,17 @@
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+
 variable "ssh_public_key" {
   type        = string
   description = "Chave publica para as VMs"
